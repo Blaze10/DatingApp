5a630eb245daac34000f3788ad86e4a3fe472250
Blaze10 <tagadechinmay@gmail.com>
2019-01-07 04:35:55 +0530

    End of section 10

--- DatingApp-SPA/src/app/app.module.ts
+++ DatingApp-SPA/src/app/app.module.ts
@@ -1,3 +1,5 @@
+import { MemberEditResolver } from './_resolvers/member-edit.resolver';
+import { MemberEditComponent } from './members/member-edit/member-edit.component';
 import { MemberListResolver } from './_resolvers/member-list.resolver.1';
 import { MemberDetailResolver } from './_resolvers/member-detail.resolver';
 import { MemberDetailComponent } from './members/member-detail/member-detail.component';
@@ -24,6 +26,7 @@ import { appRoute } from './route';
 import { JwtModule } from '@auth0/angular-jwt';
 import { TabsModule } from 'ngx-bootstrap/tabs';
 import { NgxGalleryModule } from 'ngx-gallery';
+import { PreventUnsavedChanges } from './_gaurd/prevent-unsaved-changes.guard';
 
 export function tokenGetter() {
    return localStorage.getItem('token');
@@ -39,7 +42,8 @@ export function tokenGetter() {
       ListsComponent,
       MessagesComponent,
       MemberCardComponent,
-      MemberDetailComponent
+      MemberDetailComponent,
+      MemberEditComponent
    ],
    imports: [
       BrowserModule,
@@ -64,7 +68,9 @@ export function tokenGetter() {
       AuthGuard,
       UserService,
       MemberDetailResolver,
-      MemberListResolver
+      MemberListResolver,
+      MemberEditResolver,
+      PreventUnsavedChanges
    ],
    bootstrap: [
       AppComponent
--- DatingApp-SPA/src/app/route.ts
+++ DatingApp-SPA/src/app/route.ts
@@ -1,3 +1,5 @@
+import { MemberEditResolver } from './_resolvers/member-edit.resolver';
+import { MemberEditComponent } from './members/member-edit/member-edit.component';
 import { MemberListResolver } from './_resolvers/member-list.resolver.1';
 import { MemberDetailResolver } from './_resolvers/member-detail.resolver';
 import { MemberDetailComponent } from './members/member-detail/member-detail.component';
@@ -7,6 +9,7 @@ import { MemberListComponent } from './members/member-list/member-list.component
 import { HomeComponent } from './home/home.component';
 import {Routes} from '@angular/router';
 import { ListsComponent } from './lists/lists.component';
+import { PreventUnsavedChanges } from './_gaurd/prevent-unsaved-changes.guard';
 
 export const appRoute: Routes = [
   { path: '', component: HomeComponent },
@@ -17,6 +20,8 @@ export const appRoute: Routes = [
     children: [
       { path: 'members', component: MemberListComponent, resolve: {users: MemberListResolver}},
       { path: 'members/:id', component: MemberDetailComponent, resolve: {user: MemberDetailResolver}},
+      { path: 'member/edit', component: MemberEditComponent,
+       resolve: {user: MemberEditResolver}, canDeactivate: [PreventUnsavedChanges]},
       { path: 'messages', component: MessagesComponent },
       { path: 'lists', component: ListsComponent }
     ]
